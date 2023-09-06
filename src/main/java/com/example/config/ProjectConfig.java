3e141812096f3037c782664382c96941ee9da293
Mihai <mihai_j_a@yahoo.com>
2023-09-06 12:21:23 +0300

    modified ProjectConfig

--- src/main/java/com/example/config/ProjectConfig.java
+++ src/main/java/com/example/config/ProjectConfig.java
@@ -4,10 +4,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-@EnableFeignClients(basePackages = {"com.example.getProduct", "com.example.restClient", "com.example.postProduct", "com.example.deleteProduct"})
+@EnableFeignClients(basePackages = {"com.example.restClient"})
 public class ProjectConfig {
 
 
